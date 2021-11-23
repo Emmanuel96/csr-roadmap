89cbbe8a90d7e10805d7fdcddc67527060a99fe3
borodedamie <opeoluborode@yahoo.com>
2021-11-23 00:27:28 -0800

    added the checkbox logic for the workplace activity map

--- main.js
+++ main.js
@@ -52,6 +52,60 @@ const envTotalSixthPositiveAction = document.querySelector('#sixth_option_total_
 const sixthLeadingAction = document.querySelector('#sixth_option_leading_action');
 const envTotalSixthLeadingAction = document.querySelector('#sixth_option_total_environmental_leading_action');
 
+const workplaceFirstNoAction = document.querySelector('#workplace_first_question_no_action');
+const totalWorkplaceFirstNoAction = document.querySelector('#total_workplace_question_one_no_action');
+const workplaceFirstLimitedAction = document.querySelector('#workplace_first_question_limited_action');
+const totalWorkplaceFirstLimitedAction = document.querySelector('#total_workplace_question_one_limited_action');
+const workplaceFirstPositiveAction = document.querySelector('#workplace_first_question_positive_action');
+const totalWorkplaceFirstPositiveAction = document.querySelector('#total_workplace_question_one_positive_action');
+const workplaceFirstLeadingAction = document.querySelector('#workplace_first_question_leading_action');
+const totalWorkplaceFirstLeadingAction = document.querySelector('#total_workplace_question_one_leading_action');
+
+const workplaceSecondNoAction = document.querySelector('#workplace_second_question_no_action');
+const totalWorkplaceSecondNoAction = document.querySelector('#total_workplace_question_two_no_action');
+const workplaceSecondLimitedAction = document.querySelector('#workplace_second_question_limited_action');
+const totalWorkplaceSecondLimitedAction = document.querySelector('#total_workplace_question_two_limited_action');
+const workplaceSecondPositiveAction = document.querySelector('#workplace_second_question_positive_action');
+const totalWorkplaceSecondPositiveAction = document.querySelector('#total_workplace_question_two_positive_action');
+const workplaceSecondLeadingAction = document.querySelector('#workplace_second_question_leading_action');
+const totalWorkplaceSecondLeadingAction = document.querySelector('#total_workplace_question_two_leading_action');
+
+const workplaceThirdNoAction = document.querySelector('#workplace_third_question_no_action');
+const totalWorkplaceThirdNoAction = document.querySelector('#total_workplace_question_three_no_action');
+const workplaceThirdLimitedAction = document.querySelector('#workplace_third_question_limited_action');
+const totalWorkplaceThirdLimitedAction = document.querySelector('#total_workplace_question_three_limited_action');
+const workplaceThirdPositiveAction = document.querySelector('#workplace_third_question_positive_action');
+const totalWorkplaceThirdPositiveAction = document.querySelector('#total_workplace_question_three_positive_action');
+const workplaceThirdLeadingAction = document.querySelector('#workplace_third_question_leading_action');
+const totalWorkplaceThirdLeadingAction = document.querySelector('#total_workplace_question_three_leading_action');
+
+const workplaceFourthNoAction = document.querySelector('#workplace_fourth_question_no_action');
+const totalWorkplaceFourthNoAction = document.querySelector('#total_workplace_question_four_no_action');
+const workplaceFourthLimitedAction = document.querySelector('#workplace_fourth_question_limited_action');
+const totalWorkplaceFourthLimitedAction = document.querySelector('#total_workplace_question_four_limited_action');
+const workplaceFourthPositiveAction = document.querySelector('#workplace_fourth_question_positive_action');
+const totalWorkplaceFourthPositiveAction = document.querySelector('#total_workplace_question_four_positive_action');
+const workplaceFourthLeadingAction = document.querySelector('#workplace_fourth_question_leading_action');
+const totalWorkplaceFourthLeadingAction = document.querySelector('#total_workplace_question_four_leading_action');
+
+const workplaceFifthNoAction = document.querySelector('#workplace_fifth_question_no_action');
+const totalWorkplaceFifthNoAction = document.querySelector('#total_workplace_question_five_no_action');
+const workplaceFifthLimitedAction = document.querySelector('#workplace_fifth_question_limited_action');
+const totalWorkplaceFifthLimitedAction = document.querySelector('#total_workplace_question_five_limited_action');
+const workplaceFifthPositiveAction = document.querySelector('#workplace_fifth_question_positive_action');
+const totalWorkplaceFifthPositiveAction = document.querySelector('#total_workplace_question_five_positive_action');
+const workplaceFifthLeadingAction = document.querySelector('#workplace_fifth_question_leading_action');
+const totalWorkplaceFifthLeadingAction = document.querySelector('#total_workplace_question_five_leading_action');
+
+const workplaceSixthNoAction = document.querySelector('#workplace_sixth_question_no_action');
+const totalWorkplaceSixthNoAction = document.querySelector('#total_workplace_question_six_no_action');
+const workplaceSixthLimitedAction = document.querySelector('#workplace_sixth_question_limited_action');
+const totalWorkplaceSixthLimitedAction = document.querySelector('#total_workplace_question_six_limited_action');
+const workplaceSixthPositiveAction = document.querySelector('#workplace_sixth_question_positive_action');
+const totalWorkplaceSixthPositiveAction = document.querySelector('#total_workplace_question_six_positive_action');
+const workplaceSixthLeadingAction = document.querySelector('#workplace_sixth_question_leading_action');
+const totalWorkplaceSixthLeadingAction = document.querySelector('#total_workplace_question_six_leading_action');
+
 environmentalNoAction.addEventListener('change', (e) => {
     if(environmentalNoAction.checked) {
         envTotalNoAction.disabled = false;
@@ -328,5 +382,281 @@ sixthLeadingAction.addEventListener('change', () => {
         envTotalSixthLimitedAction.checked = false;
         envTotalSixthPositiveAction.checked = false;
         envTotalSixthLeadingAction.checked = false;
+    }
+})
+
+workplaceFirstNoAction.addEventListener('change', () => {
+    if(workplaceFirstNoAction.checked) {
+        totalWorkplaceFirstNoAction.disabled = false;
+        totalWorkplaceFirstNoAction.style.backgroundColor = 'red';
+    } else {
+        totalWorkplaceFirstNoAction.disabled = true;
+        totalWorkplaceFirstNoAction.style.backgroundColor = 'white';
+    }
+})
+
+workplaceFirstLimitedAction.addEventListener('change', () => {
+    if(workplaceFirstLimitedAction.checked) {
+        totalWorkplaceFirstLimitedAction.checked = true;
+    } else {
+        totalWorkplaceFirstLimitedAction.checked = false;
+    }
+})
+
+workplaceFirstPositiveAction.addEventListener('change', () => {
+    if(workplaceFirstPositiveAction.checked) {
+        workplaceFirstLimitedAction.checked = true;
+        totalWorkplaceFirstLimitedAction.checked = true;
+        totalWorkplaceFirstPositiveAction.checked = true;
+    } else {
+        workplaceFirstLimitedAction.checked = false;
+        totalWorkplaceFirstLimitedAction.checked = false;
+        totalWorkplaceFirstPositiveAction.checked = false;
+    }
+})
+
+workplaceFirstLeadingAction.addEventListener('change', () => {
+    if(workplaceFirstLeadingAction.checked) {
+        workplaceFirstLimitedAction.checked = true;
+        workplaceFirstPositiveAction.checked = true;
+        totalWorkplaceFirstLimitedAction.checked = true;
+        totalWorkplaceFirstPositiveAction.checked = true;
+        totalWorkplaceFirstLeadingAction.checked = true;
+    } else {
+        workplaceFirstLimitedAction.checked = false;
+        workplaceFirstPositiveAction.checked = false;
+        totalWorkplaceFirstLimitedAction.checked = false;
+        totalWorkplaceFirstPositiveAction.checked = false;
+        totalWorkplaceFirstLeadingAction.checked = false;
+    }
+})
+
+workplaceSecondNoAction.addEventListener('change', () => {
+    if(workplaceSecondNoAction.checked) {
+        totalWorkplaceSecondNoAction.disabled = false;
+        totalWorkplaceSecondNoAction.style.backgroundColor = 'red';
+    } else {
+        totalWorkplaceSecondNoAction.disabled = true;
+        totalWorkplaceSecondNoAction.style.backgroundColor = 'white';
+    }
+})
+
+workplaceSecondLimitedAction.addEventListener('change', () => {
+    if(workplaceSecondLimitedAction.checked) {
+        totalWorkplaceSecondLimitedAction.checked = true;
+    } else {
+        totalWorkplaceSecondLimitedAction.checked = false;
+    }
+})
+
+workplaceSecondPositiveAction.addEventListener('change', () => {
+    if(workplaceSecondPositiveAction.checked) {
+        workplaceSecondLimitedAction.checked = true;
+        totalWorkplaceSecondLimitedAction.checked = true;
+        totalWorkplaceSecondPositiveAction.checked = true;
+    } else {
+        workplaceSecondLimitedAction.checked = false;
+        totalWorkplaceSecondLimitedAction.checked = false;
+        totalWorkplaceSecondPositiveAction.checked = false;
+    }
+})
+
+workplaceSecondLeadingAction.addEventListener('change', () => {
+    if(workplaceSecondLeadingAction.checked) {
+        workplaceSecondLimitedAction.checked = true;
+        workplaceSecondPositiveAction.checked = true;
+        totalWorkplaceSecondLimitedAction.checked = true;
+        totalWorkplaceSecondPositiveAction.checked = true;
+        totalWorkplaceSecondLeadingAction.checked = true;
+    } else {
+        workplaceSecondLimitedAction.checked = false;
+        workplaceSecondPositiveAction.checked = false;
+        totalWorkplaceSecondLimitedAction.checked = false;
+        totalWorkplaceSecondPositiveAction.checked = false;
+        totalWorkplaceSecondLeadingAction.checked = false;
+    }
+})
+
+workplaceThirdNoAction.addEventListener('change', () => {
+    if(workplaceThirdNoAction.checked) {
+        totalWorkplaceThirdNoAction.disabled = false;
+        totalWorkplaceThirdNoAction.style.backgroundColor = 'red';
+    } else {
+        totalWorkplaceThirdNoAction.disabled = true;
+        totalWorkplaceThirdNoAction.style.backgroundColor = 'white';
+    }
+})
+
+workplaceThirdLimitedAction.addEventListener('change', () => {
+    if(workplaceThirdLimitedAction.checked) {
+        totalWorkplaceThirdLimitedAction.checked = true;
+    } else {
+        totalWorkplaceThirdLimitedAction.checked = false;
+    }
+})
+
+workplaceThirdPositiveAction.addEventListener('change', () => {
+    if(workplaceThirdPositiveAction.checked) {
+        workplaceThirdLimitedAction.checked = true;
+        totalWorkplaceThirdLimitedAction.checked = true;
+        totalWorkplaceThirdPositiveAction.checked = true;
+    } else {
+        workplaceThirdLimitedAction.checked = false;
+        totalWorkplaceThirdLimitedAction.checked = false;
+        totalWorkplaceThirdPositiveAction.checked = false;
+    }
+})
+
+workplaceThirdLeadingAction.addEventListener('change', () => {
+    if(workplaceThirdLeadingAction.checked) {
+        workplaceThirdLimitedAction.checked = true;
+        workplaceThirdPositiveAction.checked = true;
+        totalWorkplaceThirdLimitedAction.checked = true;
+        totalWorkplaceThirdPositiveAction.checked = true;
+        totalWorkplaceThirdLeadingAction.checked = true;
+    } else {
+        workplaceThirdLimitedAction.checked = false;
+        workplaceThirdPositiveAction.checked = false;
+        totalWorkplaceThirdLimitedAction.checked = false;
+        totalWorkplaceThirdPositiveAction.checked = false;
+        totalWorkplaceThirdLeadingAction.checked = false;
+    }
+})
+
+workplaceFourthNoAction.addEventListener('change', () => {
+    if(workplaceFourthNoAction.checked) {
+        totalWorkplaceFourthNoAction.disabled = false;
+        totalWorkplaceFourthNoAction.style.backgroundColor = 'red';
+    } else {
+        totalWorkplaceFourthNoAction.disabled = true;
+        totalWorkplaceFourthNoAction.style.backgroundColor = 'white';
+    }
+})
+
+workplaceFourthLimitedAction.addEventListener('change', () => {
+    if(workplaceFourthLimitedAction.checked) {
+        totalWorkplaceFourthLimitedAction.checked = true;
+    } else {
+        totalWorkplaceFourthLimitedAction.checked = false;
+    }
+})
+
+workplaceFourthPositiveAction.addEventListener('change', () => {
+    if(workplaceFourthPositiveAction.checked) {
+        workplaceFourthLimitedAction.checked = true;
+        totalWorkplaceFourthLimitedAction.checked = true;
+        totalWorkplaceFourthPositiveAction.checked = true;
+    } else {
+        workplaceFourthLimitedAction.checked = false;
+        totalWorkplaceFourthLimitedAction.checked = false;
+        totalWorkplaceFourthPositiveAction.checked = false;
+    }
+})
+
+workplaceFourthLeadingAction.addEventListener('change', () => {
+    if(workplaceFourthLeadingAction.checked) {
+        workplaceFourthLimitedAction.checked = true;
+        workplaceFourthPositiveAction.checked = true;
+        totalWorkplaceFourthLimitedAction.checked = true;
+        totalWorkplaceFourthPositiveAction.checked = true;
+        totalWorkplaceFourthLeadingAction.checked = true;
+    } else {
+        workplaceFourthLimitedAction.checked = false;
+        workplaceFourthPositiveAction.checked = false;
+        totalWorkplaceFourthLimitedAction.checked = false;
+        totalWorkplaceFourthPositiveAction.checked = false;
+        totalWorkplaceFourthLeadingAction.checked = false;
+    }
+})
+
+workplaceFifthNoAction.addEventListener('change', () => {
+    if(workplaceFifthNoAction.checked) {
+        totalWorkplaceFifthNoAction.disabled = false;
+        totalWorkplaceFifthNoAction.style.backgroundColor = 'red';
+    } else {
+        totalWorkplaceFifthNoAction.disabled = true;
+        totalWorkplaceFifthNoAction.style.backgroundColor = 'white';
+    }
+})
+
+workplaceFifthLimitedAction.addEventListener('change', () => {
+    if(workplaceFifthLimitedAction.checked) {
+        totalWorkplaceFifthLimitedAction.checked = true;
+    } else {
+        totalWorkplaceFifthLimitedAction.checked = false;
+    }
+})
+
+workplaceFifthPositiveAction.addEventListener('change', () => {
+    if(workplaceFifthPositiveAction.checked) {
+        workplaceFifthLimitedAction.checked = true;
+        totalWorkplaceFifthLimitedAction.checked = true;
+        totalWorkplaceFifthPositiveAction.checked = true;
+    } else {
+        workplaceFifthLimitedAction.checked = false;
+        totalWorkplaceFifthLimitedAction.checked = false;
+        totalWorkplaceFifthPositiveAction.checked = false;
+    }
+})
+
+workplaceFifthLeadingAction.addEventListener('change', () => {
+    if(workplaceFifthLeadingAction.checked) {
+        workplaceFifthLimitedAction.checked = true;
+        workplaceFifthPositiveAction.checked = true;
+        totalWorkplaceFifthLimitedAction.checked = true;
+        totalWorkplaceFifthPositiveAction.checked = true;
+        totalWorkplaceFifthLeadingAction.checked = true;
+    } else {
+        workplaceFifthLimitedAction.checked = false;
+        workplaceFifthPositiveAction.checked = false;
+        totalWorkplaceFifthLimitedAction.checked = false;
+        totalWorkplaceFifthPositiveAction.checked = false;
+        totalWorkplaceFifthLeadingAction.checked = false;
+    }
+})
+
+workplaceSixthNoAction.addEventListener('change', () => {
+    if(workplaceSixthNoAction.checked) {
+        totalWorkplaceSixthNoAction.disabled = false;
+        totalWorkplaceSixthNoAction.style.backgroundColor = 'red';
+    } else {
+        totalWorkplaceSixthNoAction.disabled = true;
+        totalWorkplaceSixthNoAction.style.backgroundColor = 'white';
+    }
+})
+
+workplaceSixthLimitedAction.addEventListener('change', () => {
+    if(workplaceSixthLimitedAction.checked) {
+        totalWorkplaceSixthLimitedAction.checked = true;
+    } else {
+        totalWorkplaceSixthLimitedAction.checked = false;
+    }
+})
+
+workplaceSixthPositiveAction.addEventListener('change', () => {
+    if(workplaceSixthPositiveAction.checked) {
+        workplaceSixthLimitedAction.checked = true;
+        totalWorkplaceSixthLimitedAction.checked = true;
+        totalWorkplaceSixthPositiveAction.checked = true;
+    } else {
+        workplaceSixthLimitedAction.checked = false;
+        totalWorkplaceSixthLimitedAction.checked = false;
+        totalWorkplaceSixthPositiveAction.checked = false;
+    }
+})
+
+workplaceSixthLeadingAction.addEventListener('change', () => {
+    if(workplaceSixthLeadingAction.checked) {
+        workplaceSixthLimitedAction.checked = true;
+        workplaceSixthPositiveAction.checked = true;
+        totalWorkplaceSixthLimitedAction.checked = true;
+        totalWorkplaceSixthPositiveAction.checked = true;
+        totalWorkplaceSixthLeadingAction.checked = true;
+    } else {
+        workplaceSixthLimitedAction.checked = false;
+        workplaceSixthPositiveAction.checked = false;
+        totalWorkplaceSixthLimitedAction.checked = false;
+        totalWorkplaceSixthPositiveAction.checked = false;
+        totalWorkplaceSixthLeadingAction.checked = false;
     }
 })
